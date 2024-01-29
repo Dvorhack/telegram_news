ee2c3b9ceaa9298bbbbe2882b32941d92740f2db
Dvorhack <paul@serviel.fr>
2024-01-29 11:22:29 +0100

    Page update events

---
+++ event_watcher/scripts/page_update.py
@@ -0,0 +1,28 @@
+
+def event_watcher():
+    from scripts.utils import send_to_telegram
+    import time, requests, xxhash
+    
+    SITES = [
+        'https://www.synacktiv.com/publications/',
+    ]
+
+    last_page = {s: '' for s in SITES}
+
+    while True:
+        for site in SITES:
+            try:
+                r = requests.get(site)
+                the_hash = xxhash.xxh64(r.text).hexdigest()
+                print(site,the_hash)
+                if the_hash != last_page[site]:
+                    send_to_telegram(f'[page_update] {site}'.encode())
+                    last_page[site] = the_hash
+            except Exception as e:
+                send_to_telegram(f'[page_update] Error in GET {site}: {e}'.encode())
+
+        time.sleep(30 * 60) # Every 30 min
+
+
+if __name__ == "__main__":
+    event_watcher()
